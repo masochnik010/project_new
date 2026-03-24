c5fa003f1fde67673e3dcd0aa42dfd86d3fee7c3
masochnik010 <s4nssanes@gmail.com>
2026-03-24 15:26:39 +0700

    asfd

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-console.log('Hello, Hexlet!')
-
-
+console.log("Hello, Hexlet!");
+import _ from "lodash";
+_.intersection([2, 1], [2, 3]);
+_.capitalize("hello");
+console.log(_.last(["one", "two"]));
